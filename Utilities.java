049d6f29a5176d84b603a066f2b2593a0ce7b1ec
asimimtiazbhatti <asim.imtiaz.bhatti@gmail.com>
2019-07-05 17:00:55 +0500

    Utilities.java

---
+++ Utilities.java
@@ -0,0 +1,10 @@
+
+public class Utilities {
+	public static void main(String[] args) throws Exception{
+		
+		String mystr = "A~B!C";
+		String array[] = mystr.split("~");
+		System.out.println(array[1]);
+	}
+
+}
